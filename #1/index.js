009488b60fd27ff01417e3b98d59babb80faf07c
StasGamingYT <53151845+StasGamingYT@users.noreply.github.com>
2020-06-08 21:47:34 +0200

    Update index.js

--- #1/index.js
+++ #1/index.js
@@ -1,1 +1,13 @@
+const config = require("./config.json");
+const Discord = require("discord.js")
 
+const client = new Discord.Client({
+    disableEveryone: true
+});
+
+client.on("ready", () => {
+    console.log(`${client.user.username} wystartował!`);
+    client.user.setActivity('❗ Tryb Testowy ❗', { type: "WATCHING" })
+});
+
+client.login(config.token);
